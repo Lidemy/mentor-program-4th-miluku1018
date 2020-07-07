9ae57c2b94f213e161071b5a0cd09d87cc63a654
nevaeh0616 <Nevaeh.C@swzph.com>
2020-07-07 17:41:33 +0800

    week3

--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -2,8 +2,9 @@ function solve(lines) {
   const str = lines[0];
   if (str === str.split('').reverse().join('')) {
     console.log('True');
+  } else {
+    console.log('False');
   }
-  console.log('False');
 }
 
 solve(['ac']);
