d9032fe35c47d7eb3e2a2edac105c1cb2f16319f
miluku1018 <miluku1018@gmail.com>
2020-09-17 00:13:50 +0800

    finish hw2

---
+++ homeworks/week13/hw2/src/api.js
@@ -0,0 +1,24 @@
+import $ from 'jquery'
+
+export function getComments(apiUrl, siteKey, before, cb) {
+  let url = `${apiUrl}/api_comments.php?site_key=${siteKey}`
+  if (before) {
+    url += '&before=' + before
+  }
+  $.ajax({
+    // 與變數名稱同名可以不寫
+    url,
+  }).done(function(data) {
+    cb(data)
+  })
+}
+
+export function addComments(apiUrl, siteKey, data, cb) {
+  $.ajax({
+    type: 'POST',
+    url: `${apiUrl}/api_add_comments.php`,
+    data
+  }).done(function(data) {
+    cb(data)
+  })
+}
---
+++ homeworks/week13/hw2/src/templates.js
@@ -0,0 +1,24 @@
+export const cssTemplate = ' .card { margin-top: 12px; }'
+
+export function getForm(className, commentsClassName) {
+  return `
+  <div>
+  <form class="${className}">
+    <div class="form-group">
+      <label >暱稱</label>
+      <input name="nickname" class="form-control" ></input>
+    </div>
+    <div class="form-group">
+      <label >留言內容</label>
+      <textarea name="content" class="form-control"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">送出</button>
+  </form>
+  <div class="${commentsClassName}">
+  </div>
+</div>`
+}
+
+export function getLoadMoreButton(className) {
+  return `<button class="${className} btn btn-outline-primary mt-3">載入更多</button>`
+}
